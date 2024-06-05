e3210a46f436f0cf844a059b4c4b4cfa108d3c9d
Abheesta Vemuru <abheesta.vemuru@oqlo.ai>
2024-06-05 13:15:22 -0700

    Attempting to load from environment variables

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 import WebSocket, { WebSocketServer } from 'ws';
 import { randomUUID } from 'crypto';
+import bcrypt from 'bcrypt';
+import mysql from 'mysql';
+import dotenv from 'dotenv';
+
+// Load env variable
+dotenv.config();
+
+// MySQL connection
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL: ', err);
+        return;
+    }
+    console.log('Connected to MySQL');
+});
+
 const clients = new Map(); // has to be a Map instead of {} due to non-string keys
 const wss = new WebSocketServer({ port: 8080 }); // initiate a new server that listens on port 8080
 
@@ -11,9 +34,23 @@ wss.on('connection', (ws) => {
     console.log(`new connection assigned id: ${id}`);
 
     // send a message to all connected clients upon receiving a message from one of the connected clients
-    ws.on('message', (data) => {
-        console.log(`received: ${data}`);
-        serverBroadcast(`Client ${clients.get(ws)} ${data}`);
+    ws.on('message', async (data) => {
+        const message = JSON.parse(data);
+
+        if(message.type === 'login') {
+            const { username, password } = message;
+            const user = await getUserByUsername(username);
+
+            if (user && await bcrypt.compare(password, user.password_hash)) {
+                ws.send(JSON.stringify({ type: 'login_success', id, username}));
+            } else {
+                ws.send(JSON.stringify({ type: 'login_failure' }));
+            }
+        } else {
+            serverBroadcast('Client ${clients.get(ws)} ${data}');
+        }
+
+        
     });
 
     // stop tracking the client upon that client closing the connection
@@ -23,7 +60,7 @@ wss.on('connection', (ws) => {
     });
 
     // send the id back to the newly connected client
-    ws.send(`You have been assigned id ${id}`);
+    ws.send(JSON.stringify({ type: 'assign_id', id}));
 });
 
 // send a message to all the connected clients about how many of them there are every 15 seconds
@@ -40,5 +77,18 @@ function serverBroadcast(message) {
         }
     });
 }
+
+async function getUserByUsername(username) {
+    return new Promise((resolve, reject) => {
+        db.query('SELECT * FROM users WHERE username = ?', [username], (err, results) => {
+            if (err) {
+                console.error('Error fetching user:', err);
+                return reject(err);
+            }
+            resolve(results[0]);
+        });
+    });
+}
+
 
 console.log('The server is running and waiting for connections');
